bfb84fe011e8a2d6f1300535cbd73a76bcdd2dd1
dapimorales <maulanakhadafi114@gmail.com>
2025-10-27 21:10:24 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace UTS_ConcertTicket
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Forms/FormLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UTS_ConcertTicket.Forms
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
